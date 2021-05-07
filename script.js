fa7fb00c0a7971d89aa43727d68fa616e0779f87
Adam Landriault <>
2021-05-07 21:21:16 +0000

    fixed errors again

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let r, d, c, a
- r = 8
- d = 2 * 8
- c = 2 * Math.PI * 8
- a = Math.PI * (8 * 8)
+r = 8
+d = 2 * 8
+c = 2 * Math.PI * 8
+a = Math.PI * (8 * 8)
 
 document.write('Diameter = ' + d + ' units<br />')
 document.write('Circumference = ' + c + ' units<br />')
